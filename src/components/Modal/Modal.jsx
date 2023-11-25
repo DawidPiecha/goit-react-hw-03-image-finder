ebf70575335a2c57080f18b9bd85e0350ac2286b
Dawid <dawidpiecha88@gmail.com>
2023-11-25 14:28:26 +0100

    added function in Modal.jsx to close the modal only by clicking the overlay. Clicking on image cannot close the modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,14 @@
 import css from './Modal.module.css';
 
 const Modal = ({ imageUrl, onModalClose }) => {
+  //w ten sposób określam, że zamkniecie jest możliwe tylko po kliknięciu na tło, a niemożliwe po kliknięciu na duży obrazek
+  const handleModalClose = event => {
+    if (event.target === event.currentTarget) {
+      onModalClose();
+    }
+  };
   return (
-    <div className={css.Overlay} onClick={onModalClose}>
+    <div className={css.Overlay} onClick={handleModalClose}>
       <div className={css.Modal}>
         <img src={imageUrl} alt="Large " />
       </div>
